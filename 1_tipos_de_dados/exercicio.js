fb4f3638d55e533d33e94e691956f22f5f220c7a
jandoso <carol.jandoso@gmail.com>
2019-09-24 20:12:14 -0300

    exercicios aula2

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,19 @@
 //resposta aqui
+
+/*- Crie variaveis para o guardar o nome, idade, se é a primeira vez e uma lista produtos favoritos do cliente
+- Atribua valores a essas variaveis
+- Crie um objeto que irá receber os valores dessa variaveis e o imprima o objeto no console */
+
+let nome = "Caroline"
+let idade = 27
+let primeiraCompra = true 
+let produtosFavoritos = ["cervejas", "frutas", "saladas"]
+
+let cliente = {
+    nome: nome,
+    idade: idade,
+    primeiraCompra: primeiraCompra,
+    produtosFavoritos: produtosFavoritos
+}
+
+console.log(cliente)
--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,27 @@
 //resposta aqui
+
+/* - Crie uma variável que irá guardar o array com nome e valor dos produtos
+- Crie uma variável 'posicao' para guardar o produto escolhido pelo usuário
+- Crie uma variável 'porcentagem' que irá guardar qual porcentagem o usuário digitou
+- Procure no array qual produto foi escolhido pelo usuário
+- Calcule o desconto do produto
+- Printe na tela a seguinte frase: "Produto xxx com desconto é R$ xxx"
+
+Dica: você pode usar a função .toFixed(digits) para limitar o numero de casas decimais do desconto. */
+
+var r = require("readline-sync")
+
+
+let produto = [
+    {nome: "tomate", preco: 1.99},
+    {nome: "cebola", preco: 1.67}, 
+    {nome:"alho", preco: 3.00}]
+
+var posicao = parseInt(r.question("Escolha um produto entre 0 e 2: "))
+var porcentagem = parseInt(r.question("Digite uma porcentagem: "))
+
+var produtoSelecionado = produto[posicao]
+
+var desconto = produtoSelecionado.preco * (porcentagem / 100)
+
+console.log("Produto " + produtoSelecionado.nome + " com desconto é R$ " + desconto.toFixed(2))
